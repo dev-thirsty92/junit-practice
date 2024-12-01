f24139a29ea267c84a88938e5230c87c59083a11
THIRSTY <dev.thirsty92@gmail.com>
2024-12-01 23:18:05 +0900

    Comment Junit5 생애 주기 메서드

---
+++ src/test/java/com/junitpractice/Junit5LifeCycleTest.java
@@ -0,0 +1,44 @@
+package com.junitpractice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
+
+@Slf4j
+public class Junit5LifeCycleTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        String beforeAll = "beforeAll";
+        log.info("{}: 전체 테스트가 실행되기 전에 한 번 실행",beforeAll);
+    }
+
+    @AfterAll
+    static void afterAll() {
+        String afterAll = "afterAll";
+        log.info("{}: 전체 테스트가 실행된 후 한 번 실행", afterAll);
+    }
+
+    @BeforeEach
+    void setUp() {
+        String setUp = "setUp";
+        log.info("{}: 각 테스트가 실행되기 전에 실행", setUp);
+    }
+
+    @AfterEach
+    void tearDown() {
+        String tearDown = "tearDown";
+        log.info("{}: 각 테스트가 실행되기 전에 실행", tearDown);
+    }
+
+    @Test
+    void name1() {
+        String name = "test1";
+        log.info("{}: 테스트메서드1", name);
+    }
+
+    @Test
+    void name2() {
+        String name = "test2";
+        log.info("{}: 테스트메서드2", name);
+    }
+}
